ea085009d1f8ef94da940b1fb54d77bd1fe2d6ac
Athulya Jolly <athulyajolly7@gmail.com>
2025-06-10 17:18:23 +0530

    added permission,session,cookies

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('admin-dashboard/',views.admin_dashboard, name='admin_dashboard'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
+from django.http import HttpResponseForbidden
 
 def register(request):
     if request.method == 'POST':
@@ -13,7 +14,7 @@ def register(request):
             return redirect('home')
     else:
         form = RegisterForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'blog/register.html', {'form': form})
 
 
 def login_view(request):
@@ -26,14 +27,40 @@ def login_view(request):
             login(request, user)
             return redirect('home')
         else:
-            return render(request, 'login.html', {'error': 'Invalid credentials'})
+            return render(request, 'blog/login.html', {'error': 'Invalid credentials'})
 
-    return render(request, 'login.html')
+    return render(request, 'blog/login.html')
 
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'blog/home.html')
+
+
+@login_required
+def dashboard(request):
+   
+    request.session['theme'] = request.session.get('theme', 'light')
+    
+    show_popup = request.COOKIES.get('welcome_shown') != 'yes'
+    response = render(request, 'blog/dashboard.html', {
+        'show_popup': show_popup,
+        'theme': request.session['theme']
+    })
+    if show_popup:
+        # Set cookie to expire in 7 days
+        response.set_cookie('welcome_shown', 'yes', max_age=7*24*60*60)
+    return response
+
+@login_required
+def admin_dashboard(request):
+    print("Logged in user:", request.user.username)
+    print("User groups:", list(request.user.groups.values_list('name', flat=True)))
+
+    if request.user.groups.filter(name='Admins').exists():
+        return render(request, 'blog/admin_dashboard.html')
+
+    return render(request, '403.html', status=403)
 
 
 def logout_view(request):
